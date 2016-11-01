fcf6be774026e28a16b420e65e9891258c30c898
Ralph Otte <17785@ma-web.nl>
2016-11-01 09:52:42 +0100

    MatchSetup

---
+++ Assets/Scripts/MatchSetupScreen.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MatchSetupScreen : MonoBehaviour {
+
+    [SerializeField]
+    private Button hotSeatYesButton;
+    [SerializeField]
+    private Button hotSeatNoButton;
+    [SerializeField]
+    private Button playerAmountPlusButton;
+    [SerializeField]
+    private Button playerAmountMinusButton;
+    [SerializeField]
+    private Button backToMainButton;
+    [SerializeField]
+    private Button startGameButton;
+
+    [SerializeField]
+    private Text playerAmountText;
+
+    [SerializeField]
+    private int hotSeatPlayerAmount;
+    [SerializeField]
+    private int sceneToChangeTo;
+    [SerializeField]
+    private bool hotSeatMode;
+
+    void SwitchHotSeat()
+    {
+        if (hotSeatMode == false)
+        {
+            hotSeatMode = true;
+        }
+        else { hotSeatMode = false; }
+    }
+
+    void IncrementHotSeatPlayers()
+    {
+        hotSeatPlayerAmount++;
+    }
+
+    void DecrementHotSeatPlayers()
+    {
+        hotSeatPlayerAmount--;
+    }
+
+    void ConvertPlayerAmountIntToString()
+    {
+        //playerAmountText = hotSeatPlayerAmount.ToString();
+    }
+
+    void StartGame()
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
---
+++ Assets/Scripts/RoundSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundSystem : MonoBehaviour {
+
+    [SerializeField]
+    private int playerAmount;
+    [SerializeField]
+    private int hotSeatPlayerAmount;
+    [SerializeField]
+    private int whosTurn;
+
+    [SerializeField]
+    private bool hotSeatMode;
+
+    void ResetParameters()
+    {
+        playerAmount = 0;
+        hotSeatPlayerAmount = 0;
+        whosTurn = 1;
+        hotSeatMode = false;
+    }
+
+	void InitializeFirstRound () {
+        if (hotSeatMode == true)
+        {
+            playerAmount = hotSeatPlayerAmount;
+        }
+        else { playerAmount = 2; }
+        whosTurn = 1;
+	
+	}
+
+    void AdvanceTurn()
+    {
+        if (whosTurn >= playerAmount)
+        {
+            whosTurn = 1;
+        }
+        else { whosTurn++; }
+    }
+}
