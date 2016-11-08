f365df6b66f5419d0c30fca8b922661c8da89ab5
Tyrone Schumacher <tschumacher1997@gmail.com>
2016-11-08 10:17:48 +0100

    target script + menu
    target en menu finished

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;  // benodigheden voor Ui en Scene management ( laden / sluiten van scenes )
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    [SerializeField]
+     private Canvas startMenu;
+    [SerializeField]
+    private Button playButton;
+    [SerializeField]
+    private Button quitButton;
+
+
+
+
+    void Start()
+
+    {
+        startMenu = startMenu.GetComponent<Canvas>();  // aanroepen Canvassen en buttons -- Tyrone
+        playButton = playButton.GetComponent<Button>();
+        quitButton = quitButton.GetComponent<Button>();
+    }
+
+    
+    public void ProfileLevel()
+    {
+        SceneManager.LoadScene("MatchSetup");
+    }
+
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Targets.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Targets : MonoBehaviour {
+
+    [SerializeField]
+    private float score = 0f;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private bool lowTarget = false;
+    [SerializeField]
+    private bool mediumTarget = false;
+    [SerializeField]
+    private bool highTarget = false;
+
+    void Update()
+    {
+        scoreText.text = "Score:" + ((int)score).ToString();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("lowtarget"))
+        {
+            lowTarget = true;
+            mediumTarget = false;
+            highTarget = false;
+        }
+
+        if (other.CompareTag("mediumtarget"))
+        {
+            mediumTarget = true;
+            lowTarget = false;
+            highTarget = false;
+        }
+
+        if (other.CompareTag("hightarget"))
+        {
+            highTarget = true;
+            lowTarget = false;
+            mediumTarget = false;
+        }
+
+        if (other.CompareTag("outofbounds"))
+        {
+            lowTarget = false;
+            mediumTarget = false;
+            highTarget = false;
+        }
+
+        if (lowTarget == true)
+        {
+            score += 10;
+        }
+
+        if (mediumTarget == true)
+        {
+            score += 20;
+        }
+
+        if (highTarget == true)
+        {
+            score += 30;
+        }
+
+    }
+}
